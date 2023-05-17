6e1319c52d27d53c9d5e3e74ce27d09658e04774
Jack Bliss <jack@battlefy.com>
2023-05-17 10:56:16 +0100

    made links on the home page nicer to click

--- src/server/pages/routine/index.ts
+++ src/server/pages/routine/index.ts
@@ -20,17 +20,30 @@ routine.get('/:routine', async (req, res, next) => {
   }
 });
 
+const routines: { label: string; path: string }[] = [
+  { label: 'Gym warmup', path: 'warmup' },
+  { label: 'Squats', path: 'squats' },
+  { label: 'Push-ups', path: 'pushups' },
+];
+
 routine.get('/', async (req, res) => {
   const page = renderTemplate(template, {
     title: 'Routines',
-    styles: '<link rel="stylesheet" href="/bundles/routine-page.css" />',
+    styles: `<link rel="stylesheet" href="/bundles/routine-page.css" />
+    <style>
+    h4 {
+      font-size: 30px;
+      padding: 10px 0;
+    }
+    </style>`,
     body: `<h3>Routines</h3>
   <div class="not-started">
-    <ul>
-      <li><a href="/routine/warmup">Warmup</a></li>
-      <li><a href="/routine/squats">Squats</a></li>
-      <li><a href="/routine/pushups">Pushups</a></li>
-    </ul>
+  ${routines
+    .map(
+      ({ path, label }) =>
+        `<h4><a href="/routine/${path}">${label}</a></h4>`,
+    )
+    .join('\n')}
   </div>`,
   });
   return res.type('text/html').send(page);
